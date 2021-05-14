789d59d9fb0e52f80a5517588d88d65ec0700e8f
thejuicecompany <77551647+thejuicecompany@users.noreply.github.com>
2021-05-14 19:15:47 +0100

    Update main.js

--- escape/scripts/main.js
+++ escape/scripts/main.js
@@ -121,6 +121,8 @@ function init() {
         content.display = 'none';
         end.display = 'block';
         body.backgroundColor = '#89b7ce';
+        hornSound.volume = 0.6;
+        endMusic.volume = 0.6;
         hornSound.play();
         endMusic.play();
         confetti.start()
